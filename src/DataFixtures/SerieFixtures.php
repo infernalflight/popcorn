fc34265afd4b7522b1d643ae90f0018d9d78f5c6
ffotre <ffotre@campus-eni.fr>
2024-08-14 15:33:10 +0200

    Fixtures

---
+++ src/DataFixtures/SerieFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class SerieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i=0; $i < 500; $i++) {
+            $serie = new Serie();
+            $serie->setName($faker->words(7, true))
+                ->setGenres($faker->randomElement(['SF', 'Western', 'Comedy', 'Drama', 'Thriller', 'Gore']))
+                ->setFirstAirDate($faker->dateTimeBetween(new \DateTime('-10 year'), new \DateTime('-1 month')))
+                ->setStatus($faker->randomElement(['Ended', 'Returning', 'Canceled']))
+                ->setVote($faker->randomFloat(1, 0, 10))
+                ->setPopularity($faker->randomFloat(2, 0, 1000))
+                ->setDateCreated(new \DateTime())
+            ;
+
+            if (\in_array($serie->getStatus(), ['Ended', 'Canceled'])) {
+                $serie->setLastAirDate($faker->dateTimeBetween($serie->getFirstAirDate(), new \DateTime()));
+            }
+
+            $manager->persist($serie);
+        }
+
+        $manager->flush();
+    }
+}
