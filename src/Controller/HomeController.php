690d2c8bf3e94224796b4060ab0391b2460278ff
infernalflight <ffotre@campus-eni.fr>
2024-09-02 14:47:39 +0200

    Consommation API

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HttpClientInterface $httpClient): Response
     {
+
+        //$response = $httpClient->request('GET', 'https://api.api-ninjas.com/v1/chucknorris');
+        //$joke = json_decode($response->getContent(), true)['value'];
 
         return $this->render('home/index.html.twig');
     }
